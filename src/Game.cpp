d07bc69cb27a9b5c9498e634e01d05e1fe107a71
mmohamedali <sugaal@gmail.com>
2019-07-21 20:09:05 -0400

    Added game dependencies and structure

---
+++ src/Game.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
+
+Game::Game() {
+    this->isRunning = false;
+}
+
+Game::~Game() {
+}
+
+bool Game::IsRunning() const {
+    return this->isRunning;
+}
+
+float projectilePosX = 0.0f;
+float projectilePosY = 0.0f;
+float projectileVelX = 0.1f;
+float projectileVelY = 0.1f;
+
+void Game::Initialize(int width, int height) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    window = SDL_CreateWindow(
+            NULL,
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            width,
+            height,
+            SDL_WINDOW_BORDERLESS
+    );
+    if (!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+        return;
+    }
+
+    isRunning = true;
+    return;
+}
+
+void Game::ProcessInput() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT: {
+            isRunning = false;
+            break;
+        }
+        case SDL_KEYDOWN: {
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                isRunning = false;
+            }
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+void Game::Update() {
+    projectilePosX += projectileVelX;
+    projectilePosY += projectileVelY;
+}
+
+void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_Rect projectile = {
+            (int) projectilePosX,
+            (int) projectilePosY,
+            10,
+            10
+    };
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &projectile);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Destroy() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
---
+++ src/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
+
+int main(int argc, char *args[]) {
+    Game *game = new Game();
+
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while (game->IsRunning()) {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+
+    game->Destroy();
+
+    return 0; 
+}
